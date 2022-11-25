b64321899808e09b7914073ff1f4973a06657e78
bogdanperley <88403935+bogdanperley@users.noreply.github.com>
2022-11-25 21:39:58 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,21 +1,18 @@
-﻿#include<string>
+﻿#include <string>
+#include "Algoritm.h"
 #include <iostream>
-#include"Algoritm.h"
 
+int main() {
+  Concrete_builder_1 con_1;
+  build(con_1);
+  con_1.step_1();
+  std::string str_1 = con_1.get_Result();
+  std::cout << str_1 << std::endl;
 
-int main()
-{
-	Concrete_builder_1 con_1;
-	build(con_1);
-	con_1.step_1();
-	std::string str_1 = con_1.get_Result();
-	std::cout << str_1 << std::endl;
+  Concrete_builder_2 con_2;
+  build(con_2);
+  std::string str_2 = con_2.get_Result();
+  std::cout << str_2 << std::endl;
 
-	Concrete_builder_2 con_2;
-	build(con_2);
-	std::string str_2 = con_2.get_Result();
-	std::cout << str_2 << std::endl;
-
-
-	return 0;
+  return 0;
 }
